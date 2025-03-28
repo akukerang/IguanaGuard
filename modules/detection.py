1401c9335abdc57c7ff2404507bf1edef6b09d27
pi gabe <gabrielsuoth@gmail.com>
2025-03-28 17:59:04 -0400

    Detection Module

---
+++ modules/detection.py
@@ -0,0 +1,53 @@
+import modules.camera as cam
+from hailo_platform import (HEF, VDevice, HailoStreamInterface, InferVStreams, ConfigureParams,
+    InputVStreamParams, OutputVStreamParams, InputVStreams, OutputVStreams, FormatType) 
+import numpy as np
+
+class Detection_Model:
+    def __init__(self, confidence=0.5):
+        #=== Hailo Setup ===
+        self.target = VDevice()
+        self.CONFIDENCE_THRESHOLD = confidence
+        self.hef_path = 'models/unique_yolov8n.hef'
+        self.hef = HEF(self.hef_path)
+
+        # Configure network groups
+        self.configure_params = ConfigureParams.create_from_hef(hef=self.hef, interface=HailoStreamInterface.PCIe)
+        self.network_groups = self.target.configure(self.hef, self.configure_params)
+        self.network_group = self.network_groups[0]
+        self.network_group_params = self.network_group.create_params()
+
+        # Create input and output virtual streams params
+        self.input_vstreams_params = InputVStreamParams.make(self.network_group, format_type=FormatType.FLOAT32)
+        self.output_vstreams_params = OutputVStreamParams.make(self.network_group, format_type=FormatType.FLOAT32)
+
+        # Define dataset params
+        self.input_vstream_info = self.hef.get_input_vstream_infos()[0]
+        self.output_vstream_info = self.hef.get_output_vstream_infos()[0]
+        self.image_height, self.image_width, self.channels = self.input_vstream_info.shape
+
+        self.undistort_frame = ""
+        self.center_x = 0
+        self.center_y = 0
+        self.detected = False
+
+    
+    def run(self, frame):
+        self.undistorted_frame = cam.undistort_frame(frame) # Remove fisheye effect        
+        preprocessed = cam.preprocess(self.undistorted_frame)
+        dim = 640
+        with InferVStreams(self.network_group, self.input_vstreams_params, self.output_vstreams_params) as infer_pipeline:
+            with self.network_group.activate(self.network_group_params):
+                infer_results = infer_pipeline.infer(preprocessed)  
+                oh, ow, _ = self.undistorted_frame.shape
+                rh, rw = oh / dim, ow / dim
+                detections = np.array(infer_results[list(infer_results.keys())[0]][0])
+                self.detected = False
+                if detections[0].shape[0] > 0: # Iguana detected in frame
+                    flattened_detections = detections[0].reshape(-1, 5)
+                    sorted_detections = flattened_detections[flattened_detections[:, 4].argsort()[::-1]] # Gets box with highest confidence
+                    highest_conf = sorted_detections[0]
+                    if highest_conf[4] > self.CONFIDENCE_THRESHOLD:
+                        self.detected = True 
+                        bbox = highest_conf[0:4] * dim
+                        self.center_x, self.center_y = cam.draw_detection(self.undistorted_frame, bbox, highest_conf[4], (0,255,0), rw, rh) # Draws bounding boxes
--- server.py
+++ server.py
@@ -1,37 +1,11 @@
 import cv2
-from multiprocessing import Process
-from hailo_platform import (HEF, VDevice, HailoStreamInterface, InferVStreams, ConfigureParams,
-    InputVStreamParams, OutputVStreamParams, InputVStreams, OutputVStreams, FormatType) 
-import numpy as np
-import RPi.GPIO as GPIO
 import time
 from flask import Flask, Response, render_template, request
 import os
 import psutil
-import modules.camera as cam
 from modules.escalation import EscalationManager
 from modules.servos import ServoController
-
-#=== Hailo Setup ===
-target = VDevice()
-CONFIDENCE_THRESHOLD = 0.50
-hef_path = 'models/unique_yolov8n.hef'
-hef = HEF(hef_path)
-
-# Configure network groups
-configure_params = ConfigureParams.create_from_hef(hef=hef, interface=HailoStreamInterface.PCIe)
-network_groups = target.configure(hef, configure_params)
-network_group = network_groups[0]
-network_group_params = network_group.create_params()
-
-# Create input and output virtual streams params
-input_vstreams_params = InputVStreamParams.make(network_group, format_type=FormatType.FLOAT32)
-output_vstreams_params = OutputVStreamParams.make(network_group, format_type=FormatType.FLOAT32)
-
-# Define dataset params
-input_vstream_info = hef.get_input_vstream_infos()[0]
-output_vstream_info = hef.get_output_vstream_infos()[0]
-image_height, image_width, channels = input_vstream_info.shape
+from modules.detection import Detection_Model
 
 # ===Flask Section===
 app = Flask(__name__)
@@ -55,38 +29,21 @@ DEBOUNCE_FRAMES = 26 # around half a second
 def generate_frames():
     camera = cv2.VideoCapture(0)
     previous_x, previous_y = None, None
-    dim = 640
     last_move_time = time.time()
     detection_count = 0 # Frame counters for debounce 
     no_detection_count = 0
     escalation_manager = EscalationManager()
     servo_controller = ServoController()
+    model  = Detection_Model()
     escalation_manager.start()
     movement_detected = False  # Detection State
     while camera.isOpened():
         ret, frame = camera.read()
         if not ret:
             break
-        undistorted_frame = cam.undistort_frame(frame) # Remove fisheye effect        
-        preprocessed = cam.preprocess(undistorted_frame)
+        model.run(frame)
 
-        with InferVStreams(network_group, input_vstreams_params, output_vstreams_params) as infer_pipeline:
-            with network_group.activate(network_group_params):
-                infer_results = infer_pipeline.infer(preprocessed)  
-                oh, ow, _ = undistorted_frame.shape
-                rh, rw = oh / dim, ow / dim
-                detections = np.array(infer_results[list(infer_results.keys())[0]][0])
-                detected = False
-                if detections[0].shape[0] > 0: # Iguana detected in frame
-                    flattened_detections = detections[0].reshape(-1, 5)
-                    sorted_detections = flattened_detections[flattened_detections[:, 4].argsort()[::-1]] # Gets box with highest confidence
-                    highest_conf = sorted_detections[0]
-                    if highest_conf[4] > CONFIDENCE_THRESHOLD:
-                        detected = True 
-                        bbox = highest_conf[0:4] * dim
-                        center_x, center_y = cam.draw_detection(undistorted_frame, bbox, highest_conf[4], (0,255,0), rw, rh) # Draws bounding boxes
-
-        if detected:
+        if model.detected:
             if not movement_detected:  # Only trigger once per detection sequence
                 escalation_manager.detect_movement()
                 movement_detected = True  # Mark as triggered
@@ -94,10 +51,10 @@ def generate_frames():
             detection_count = min(detection_count + 1, DEBOUNCE_FRAMES)
             no_detection_count = 0
 
-            if previous_x is None or abs(center_x - previous_x) > 25 or abs(center_y - previous_y) > 25:
+            if previous_x is None or abs(model.center_x - previous_x) > 25 or abs(model.center_y - previous_y) > 25:
                 # servo.move_servos(center_x, center_y)
-                servo_controller.move_servos(center_x, center_y)
-                previous_x, previous_y = center_x, center_y
+                servo_controller.move_servos(model.center_x, model.center_y)
+                previous_x, previous_y = model.center_x, model.center_y
 
         else:
             no_detection_count += 1
@@ -110,7 +67,7 @@ def generate_frames():
                 movement_detected = False  # Reset flag
             no_detection_count = DEBOUNCE_FRAMES  # Cap counter
 
-        ret, buffer = cv2.imencode('.jpg', undistorted_frame)
+        ret, buffer = cv2.imencode('.jpg', model.undistorted_frame)
         frame = buffer.tobytes()
 
         yield (b'--frame\r\n'
@@ -124,18 +81,12 @@ def generate_frames():
 def index():
     return render_template('index.html', temperature=get_cpu_temperature(), cpu_usage=get_cpu_usage())
 
-
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-def cleanup(): # Cleans up after app ends
-    GPIO.cleanup()
 
 if __name__ == '__main__':
     try:
         app.run(host='0.0.0.0', port=5000, debug=False) 
     except Exception as e:
         print(f"Error occurred: {e}")
-    finally:
-        cleanup()
